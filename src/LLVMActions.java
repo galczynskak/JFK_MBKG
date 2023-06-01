4cb3575c57c2d304b65e2ec05c9ba0dba8ab0bc7
galczynskak <galczynska.karolina@gmail.com>
2023-06-02 01:08:53 +0200

    Dodane operacje arytmetyczne

--- src/LLVMActions.java
+++ src/LLVMActions.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 class Value {
     public String type;
@@ -41,17 +37,16 @@ public class LLVMActions extends MBKGBaseListener {
         } catch (NullPointerException e) {
             ID = ctx.declaration().getChild(1).getText();
         }
-        if(!variables.containsKey(ID)){
+        if (!variables.containsKey(ID)) {
             error(ctx.getStart().getLine(), "variable not declared");
         }
 
         Value v = stack.pop();
-        System.out.println(v.type + ", " + v.value);
-        if(!v.type.equals(variables.get(ID))) {
+        if (!v.type.equals(variables.get(ID))) {
             error(ctx.getStart().getLine(), "assignment type mismatch");
         }
-        if(v.type.equals("int")) LLVMGenerator.assignInt(ID, v.value);
-        if(v.type.equals("real)")) LLVMGenerator.assignFloat(ID, v.value);
+        if (v.type.equals("int")) LLVMGenerator.assignInt(ID, v.value);
+        if (v.type.equals("real)")) LLVMGenerator.assignFloat(ID, v.value);
     }
 
     @Override
@@ -59,10 +54,10 @@ public class LLVMActions extends MBKGBaseListener {
         String ID = ctx.ID().getText();
         String TYPE = ctx.type().getText();
         if (!variables.containsKey(ID)) {
-            if (types.contains(TYPE)){
+            if (types.contains(TYPE)) {
                 variables.put(ID, TYPE);
-                if(TYPE.equals("int")) LLVMGenerator.declareInt(ID);
-                else if(TYPE.equals("float")) LLVMGenerator.declareFloat(ID);
+                if (TYPE.equals("int")) LLVMGenerator.declareInt(ID);
+                else if (TYPE.equals("float")) LLVMGenerator.declareFloat(ID);
             } else {
                 ctx.getStart().getLine();
                 System.err.println("Line " + ctx.getStart().getLine() + ", unknown variable type: " + TYPE);
@@ -76,15 +71,15 @@ public class LLVMActions extends MBKGBaseListener {
     @Override
     public void exitFunction_call(MBKGParser.Function_callContext ctx) {
         String FUNC_NAME = ctx.function().getText();
-        if(FUNC_NAME.equals("print")) {
-            if(argumentsList.size() == 1) {
+        if (FUNC_NAME.equals("print")) {
+            if (argumentsList.size() == 1) {
                 Value argument = argumentsList.get(0);
                 String ID = argument.value;
                 String type = variables.get(ID);
                 if (type != null) {
-                    if(type.equals("int")){
+                    if (type.equals("int")) {
                         LLVMGenerator.printInt(ID);
-                    } else if (type.equals("float")){
+                    } else if (type.equals("float")) {
                         LLVMGenerator.printFloat(ID);
                     }
                 } else {
@@ -92,30 +87,56 @@ public class LLVMActions extends MBKGBaseListener {
                     System.err.println("Line " + ctx.getStart().getLine() + ", unknown variable: " + ID);
                 }
             } else {
-                ctx.getStart().getLine();
-                System.err.println("Line " + ctx.getStart().getLine() + ", too many arguments in function print. Expected 1, got " + argumentsList.size());
+                if (argumentsList.size() == 0) {
+                    ctx.getStart.getLine();
+                    System.err.println("line " + ctx.getStart().getLine() + ", no arguments in function print, Expected 1, got 0");
+                } else {
+                    ctx.getStart.getLine();
+                    System.err.println("line " + ctx.getStart().getLine() + ", too many argument in function print, Expected 1, got: " + argumentsList.size());
+                }
             }
         } else if (FUNC_NAME.equals("scan")) {
-            if(argumentsList.size() == 1) {
+            if (argumentsList.size() == 1) {
                 Value argument = argumentsList.get(0);
                 String ID = argument.value;
                 String type = variables.get(ID);
-                if(type != null){
-                    if(type.equals("int")) {
+                if (type != null) {
+                    if (type.equals("int")) {
                         LLVMGenerator.scanInt(ID);
-                    } else if (type.equals("float")){
+                    } else if (type.equals("float")) {
                         LLVMGenerator.scanFloat(ID);
                     }
-                }else {
+                } else {
                     ctx.getStart().getLine();
                     System.err.println("Line " + ctx.getStart().getLine() + ", unknown variable: " + ID);
                 }
             } else {
-                ctx.getStart().getLine();
-                System.err.println("line " + ctx.getStart().getLine() + ", too many argument in function scan, Expected 1, got: " + argumentsList.size());
+                if (argumentsList.size() == 0) {
+                    ctx.getStart.getLine();
+                    System.err.println("line " + ctx.getStart().getLine() + ", no arguments in function scan, Expected 1, got 0");
+                } else {
+                    ctx.getStart.getLine();
+                    System.err.println("line " + ctx.getStart().getLine() + ", too many argument in function scan, Expected 1, got: " + argumentsList.size());
+                }
+
             }
         }
         argumentsList.clear();
+    }
+
+    @Override
+    public void exitSingle5(MBKGParser.Single5Context ctx){
+        try {
+            argumentsList.add(new Value("ID", ctx.ID().getText()));
+        } catch (NullPointerException e) {}
+
+        try {
+            argumentsList.add(new Value("int", ctx.INT().getText()));
+        } catch (NullPointerException e) {}
+
+        try {
+            argumentsList.add(new Value("float", ctx.FLOAT().getText()));
+        } catch (NullPointerException e) {}
     }
 
     @Override
@@ -128,22 +149,95 @@ public class LLVMActions extends MBKGBaseListener {
         stack.push(new Value("float", ctx.FLOAT().getText()));
     }
 
-    @Override 
+    @Override
     public void exitId(MBKGParser.IdContext ctx) {
         String ID = ctx.ID().getText();
-        if( variables.containsKey(ID) ){
+        if (variables.containsKey(ID)) {
             String type = variables.get(ID);
             int reg = -1;
-            if(type.equals("int")){
+            if (type.equals("int")) {
                 reg = LLVMGenerator.loadInt(ID);
-            } else if (type.equals("real")){
+            } else if (type.equals("real")) {
                 reg = LLVMGenerator.loadReal(ID);
             }
-            stack.push( new Value(type, "%"+reg));
+            stack.push(new Value(type, "%" + reg));
         } else {
             error(ctx.getStart().getLine(), "no such variable");
         }
     }
+
+    @Override
+    public void exitAdd(MBKGParser.AddContext ctx) {
+        Value v1 = stack.pop();
+        Value v2 = stack.pop();
+        if (v1.type.equals(v2.type)) {
+            if (v1.type.equals("int")) {
+                LLVMGenerator.addInt(v1.value, v2.value);
+                stack.push(new Value("int", "%" + (LLVMGenerator.reg - 1)));
+            }
+            if (v1.type.equals("float")) {
+                LLVMGenerator.addFloat(v1.value, v2.value);
+                stack, push(new Value("real", "%" + (LLVMGenerator.reg - 1)));
+            }
+        } else {
+            error(ctx.getStart().getLine(), "addition type mismatch");
+        }
+    }
+
+    @Override
+    public void exitSub(MBKGParser.SubContext ctx) {
+        Value v1 = stack.pop();
+        Value v2 = stack.pop();
+        if (v1.type.equals(v2.type)) {
+            if (v1.type.equals("int")) {
+                LLVMGenerator.subInt(v1.value, v2.value);
+                stack.push(new Value("int", "%" + (LLVMGenerator.reg - 1)));
+            }
+            if (v1.type.equals("float")) {
+                LLVMGenerator.subFloat(v1.value, v2.value);
+                stack, push(new Value("real", "%" + (LLVMGenerator.reg - 1)));
+            }
+        } else {
+            error(ctx.getStart().getLine(), "subtraction type mismatch");
+        }
+    }
+
+    @Override
+    public void exitMult(MBKGParser.MultContext ctx) {
+        Value v1 = stack.pop();
+        Value v2 = stack.pop();
+        if (v1.type.equals(v2.type)) {
+            if (v1.type.equals("int")) {
+                LLVMGenerator.multInt(v1.value, v2.value);
+                stack.push(new Value("int", "%" + (LLVMGenerator.reg - 1)));
+            }
+            if (v1.type.equals("float")) {
+                LLVMGenerator.multFloat(v1.value, v2.value);
+                stack, push(new Value("real", "%" + (LLVMGenerator.reg - 1)));
+            }
+        } else {
+            error(ctx.getStart().getLine(), "multiplication type mismatch");
+        }
+    }
+
+    @Override
+    public void exitDiv(MBKGParser.DivContext ctx) {
+        Value v1 = stack.pop();
+        Value v2 = stack.pop();
+        if (v1.type.equals(v2.type)) {
+            if (v1.type.equals("int")) {
+                LLVMGenerator.divInt(v1.value, v2.value);
+                stack.push(new Value("int", "%" + (LLVMGenerator.reg - 1)));
+            }
+            if (v1.type.equals("float")) {
+                LLVMGenerator.divFloat(v1.value, v2.value);
+                stack, push(new Value("real", "%" + (LLVMGenerator.reg - 1)));
+            }
+        } else {
+            error(ctx.getStart().getLine(), "division type mismatch");
+        }
+    }
+
 
     private void error(int location, String msg) {
         System.out.println("Error at line " + location + ": " + msg);
--- src/LLVMGenerator.java
+++ src/LLVMGenerator.java
@@ -4,59 +4,100 @@ public class LLVMGenerator {
     static String main_text = "";
     static int reg = 1;
 
-    static void assignInt(String id, String value){
-        main_text += "store i32 "+value+", i32* %"+id+"\n";
+    static void assignInt(String id, String value) {
+        main_text += "store i32 " + value + ", i32* %" + id + "\n";
     }
 
-    static void assignFloat(String id, String value){
-        main_text += "store double "+value+", double* %"+id+"\n";
+    static void assignFloat(String id, String value) {
+        main_text += "store double " + value + ", double* %" + id + "\n";
     }
 
-    static void declareInt(String id){
-        main_text += "%"+id+" = alloca i32\n";
+    static void declareInt(String id) {
+        main_text += "%" + id + " = alloca i32\n";
     }
 
-    static void declareFloat(String id){
-        main_text += "%"+id+" alloca i32\n";
+    static void declareFloat(String id) {
+        main_text += "%" + id + " alloca i32\n";
     }
 
-    static void printInt(String id){
-        main_text += "%"+reg+" load i32, i32* %"+id+"\n";
+    static void printInt(String id) {
+        main_text += "%" + reg + " load i32, i32* %" + id + "\n";
         reg++;
-        main_text += "%"+reg+" call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @strp, i32 0, i32 0), i32 %"+(reg-1)+")\n";
-        reg++;
-    }
-
-    static void printFloat(String id){
-        main_text += "%"+reg+" =load double, double* %"+id+"\n";
-        reg++;
-        main_text += "%"+reg+" = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @strpd, i32 0, i32 0), double %"+(reg-1)+")\n";
+        main_text += "%" + reg + " call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @strp, i32 0, i32 0), i32 %" + (reg - 1) + ")\n";
         reg++;
     }
 
-    static void scanInt(String id){
-        main_text += "%"+reg+" = call i32 (i8*, ...) @__isoc99_scanf(i8* getelementptr inbounds ([3 x i8],, [3 x i8]* @strs, i32 0, i32 0), i32* %"+id+")\n";
+    static void printFloat(String id) {
+        main_text += "%" + reg + " =load double, double* %" + id + "\n";
+        reg++;
+        main_text += "%" + reg + " = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @strpd, i32 0, i32 0), double %" + (reg - 1) + ")\n";
         reg++;
     }
 
-    static void scanFloat(String id){
-        main_text += "%"+reg+" = call i32 (i8*, ...) @__isoc99_scanf(i8* getelementpts inbounds ([4 x i8], [4 x i8]* @strd, i320, i320), double* %"+id+")\n";
+    static void scanInt(String id) {
+        main_text += "%" + reg + " = call i32 (i8*, ...) @__isoc99_scanf(i8* getelementptr inbounds ([3 x i8],, [3 x i8]* @strs, i32 0, i32 0), i32* %" + id + ")\n";
         reg++;
     }
 
-    static int loadInt(String id){
-      main_text += "%"+reg+" = load i32, i32* %"+id+"\n";
-      reg++;
-      return reg-1;
-   }
+    static void scanFloat(String id) {
+        main_text += "%" + reg + " = call i32 (i8*, ...) @__isoc99_scanf(i8* getelementpts inbounds ([4 x i8], [4 x i8]* @strd, i320, i320), double* %" + id + ")\n";
+        reg++;
+    }
 
-   static int loadReal(String id){
-      main_text += "%"+reg+" = load double, double* %"+id+"\n";
-      reg++;
-      return reg-1;
-   }
 
-    static String generate(){
+    static void addInt(String v1, String v2) {
+        main_text += "%" + reg + " = add i32 " + v1 + ", " + v2 + "\n";
+        reg++;
+    }
+
+    static void addFloat(String v1, String v2) {
+        main_text += "%" + reg + " = fadd double " + v1 + ", " + v2 + "\n";
+        reg++;
+    }
+
+    static void subInt(String v1, String v2) {
+        main_text += "%" + reg + " = sub i32 " + v1 + ", " + v2 + "\n";
+        reg++;
+    }
+
+    static void subFloat(String v1, String v2) {
+        main_text += "%" + reg + " = fsub double " + v1 + ", " + v2 + "\n";
+        reg++;
+    }
+
+    static void multInt(String v1, String v2) {
+        main_text += "%" + reg + " = mul i32 " + v1 + ", " + v2 + "\n";
+        reg++;
+    }
+
+    static void multFloat(String v1, String v2) {
+        main_text += "%" + reg + " = fmul double " + v1 + ", " + v2 + "\n";
+        reg++;
+    }
+
+    static void divInt(String v1, String v2) {
+        main_text += "%" + reg + " = sdiv i32 " + v1 + ", " + v2 + "\n";
+        reg++;
+    }
+
+    static void divFloat(String v1, String v2) {
+        main_text += "%" + reg + " = fdiv double " + v1 + ", " + v2 + "\n";
+        reg++;
+    }
+
+    static int loadInt(String id) {
+        main_text += "%" + reg + " = load i32, i32* %" + id + "\n";
+        reg++;
+        return reg - 1;
+    }
+
+    static int loadReal(String id) {
+        main_text += "%" + reg + " = load double, double* %" + id + "\n";
+        reg++;
+        return reg - 1;
+    }
+
+    static String generate() {
         String text = "";
         text += "declare i32 @printf(i8*, ...)\n";
         text += "declare i32 @__isoc99_scanf(i8*, ...)\n";
